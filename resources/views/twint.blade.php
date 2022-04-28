640027064a609653043f9e6abaf8da97de2c0f51
Sebastian Sirbu <sesirbu@gmail.com>
2022-04-28 17:28:08 +0200

    Formulario busqueda

--- resources/views/twint.blade.php
+++ resources/views/twint.blade.php
@@ -4,13 +4,15 @@
             {{ __('Twint') }}
         </h2>
     </x-slot>
-    <div class="py-12">
-        <div class="max-w-screen-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                @include('twint.panel')
-                </div>
+    <div class="grid grid-rows-2 grid-flow-col gap-4 mt-5" id="twint">
+        <div class="row-span-3 col-span-1">
+            <div class="px-4 sm:px-0">
+                <h3 class="text-lg font-medium leading-6 text-gray-900">Aqui ira el side bar</h3>
             </div>
+        </div>
+
+        <div class="row-span-3 col-span-3">
+                @include('twint.panel')
         </div>
     </div>
 </x-app-layout>
--- resources/views/twint/panel.blade.php
+++ resources/views/twint/panel.blade.php
@@ -1,73 +1,168 @@
-<div class="container-fluid">
-            <div class="" style="margin-top:100px">
-            <!-- <div class="shadow mb-5"> -->
-                <div class="rounded d-flex justify-content-center">
-           
-                    <div class="col-md-6 m-3 bg-light.bg-gradient">
-
-                        <form method="POST">
-                            <div class="p-4">
-								 <!-- style="color:#2c7b88" -->
-
-                                <div class="input-group mb-3">
-                                    <span class="input-group-text" style="background-color:#2c7b88"><i
-                                            class="bi bi-person-fill text-white"></i></span>
-                                    <input type="text" class="form-control" name="username" placeholder="Nombre de usuario">
-                                </div>
-
-                                <div class="input-group mb-3">
-                                    <span class="input-group-text" style="background-color:#2c7b88"><i
-                                            class="bi bi-chat-quote-fill text-white"></i></span>
-                                    <input type="text" class="form-control" name="palabra" placeholder="Palabras a buscar">
-                                </div>
-								
-								<div class="input-group mb-3">
-                                    <span class="input-group-text "style="background-color:#2c7b88"><i
-                                            class="bi bi-star-fill text-white"></i></span>
-                                    <input type="text" class="form-control" name="sitio" placeholder="Sitio Cercano">
-                                </div>
-
-								<div class="input-group mb-3">
-                                    <span class="input-group-text "style="background-color:#2c7b88"><i
-                                            class="bi bi-geo-alt-fill text-white"></i></span>
-                                    <input type="text" class="form-control" name="geo" placeholder="Coordenadas,Radio">
-                                </div>
-
-                                
-                                <div class="input-group mb-3">
-                                    <span class="input-group-text "style="background-color:#2c7b88"><i
-                                            class="bi bi-calendar text-white"></i></span>
-                                    <input type="date" class="form-control" name="fechaini" placeholder="Fecha inicio">
-                                </div>
-								
-								<div class="input-group mb-3">
-                                    <span class="input-group-text "style="background-color:#2c7b88"><i
-                                            class="bi bi-calendar text-white"></i></span>
-                                    <input type="date" class="form-control" name="fechafin" placeholder="Fecha fin">
-                                </div>    
-
-                                <div class="input-group mb-3">
-                                    <span class="input-group-text "style="background-color:#2c7b88"><i
-                                            class="bi bi-arrow-right-square text-white"></i></span>
-                                    <input type="number" class="form-control" name="maximo" value="10">
-                                </div>
-								 
-								
-								<div class="input-group ">
-                                    <input type="checkbox" class="btn-check" name="email" id="btn-check-email" value="1" unchecked>
-									<label class="btn btn-outline-warning" for="btn-check-email"><i class="bi bi-envelope">  &nbsp;</i>   Email</label>	
-                                &nbsp;
-								&nbsp;
-								&nbsp;
-									<input type="checkbox" class="btn-check" name="phone" id="btn-check-phone" value="1" unchecked>
-									<label class="btn btn-outline-warning " for="btn-check-phone"><i class="bi bi-telephone"> &nbsp;</i>   Phone</label>
-								</div>
-								
-								
-								
-                                <button class="btn text-center mt-5 text-white" style="background-color:#2c7b88" name="search" value="1" type="submit">
-                                <i class="bi bi-search "> &nbsp;</i>  Search
-                                </button>                          
-                            </div>
-                        </form>
-                    </div>
+<form action="#" method="POST">
+    <div class="shadow sm:rounded-md sm:overflow-hidden">
+     <div class="grid grid-rows-2 grid-flow-col gap-4">
+     <div class="row-span-4 col-span-1">
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="username" class="block text-sm font-medium text-gray-700"> NOMBRE DE USUARIO </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> 
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg> 
+                    </span>
+                  <input type="text" name="username" id="username" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Nombre de usuario">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="palabra" class="block text-sm font-medium text-gray-700"> PALABRAS A BUSCAR </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                  </svg>
+                  </span>
+                  <input type="text" name="palabra" id="palabra" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Palabras a buscar">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="sitio" class="block text-sm font-medium text-gray-700"> SITIO CERCANO </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> 
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+                  </svg>       
+                </span>
+                  <input type="text" name="sitio" id="sitio" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Sitio Cercano">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="geo" class="block text-sm font-medium text-gray-700"> COORDENADAS, RADIO </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> 
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                 </svg>
+                </span>
+                  <input type="text" name="geo" id="geo" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Coordenadas,Radio">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="fechaini" class="block text-sm font-medium text-gray-700"> FECHA INICIO </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> 
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>      
+                </span>
+                  <input type="date" name="fechaini" id="fechaini" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Fecha inicio">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="fechafin" class="block text-sm font-medium text-gray-700"> FECHA FIN </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> 
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>      
+                </span>
+                  <input type="date" name="fechafin" id="fechafin" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Fecha fin">
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="maximo" class="block text-sm font-medium text-gray-700"> NUMERO DE ENTRADAS </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> 
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  </span>
+                  <input type="number" name="maximo" id="maximo"  value="10" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300">
+                </div>
+              </div>
+            </div>
+        </div>
+        <style>
+    .boton-check {
+        background: white !important; 
+        color: black !important; 
+        border: 1px solid #2c7b88 !important;
+        box-shadow:none !important;
+        color:#2c7b88;
+    }
+   .boton-check :hover{
+        background-color:#2c7b88 !important; 
+        color:white !important;
+        border: none !important;
+    }
+    input[type="checkbox"]:checked + label{
+        background-color:#2c7b88 !important; 
+        color:white !important;
+        border: none !important;
+    } 
+    input[type="checkbox"]{
+        visibility:hidden;
+    }
+        </style>
+        <div class="bg-white">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <div class="flex rounded-md shadow-sm">
+                <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" 
+            type="checkbox" id="btn-check-email" name="email" value="1">
+            <label class="boton-check inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" for="btn-check-email">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+            </label>
+            <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" id="btn-check-phone" value="1">
+            <label class="boton-check inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" for="btn-check-phone">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+            </svg>
+            </label>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="bg-white sm:p-6">
+        <button type="submit" style="background-color:#2c7b88" class="inline-block px-6 pt-2.5 pb-2 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
+            name="search">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+             Search
+            </button>
+        </div>
+    </div>   
+</form>
+<div class="row-span-4 col-span-1">
+        <div>
+        </div>
+    </div> 
