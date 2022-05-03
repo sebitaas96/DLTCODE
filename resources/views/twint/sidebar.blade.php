73fdb690c9be73b0dcb6764d72911ea6eb8f580c
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2022-05-03 09:46:21 +0200

    Main page y side bar

---
+++ resources/views/twint/sidebar.blade.php
@@ -0,0 +1,19 @@
+    <!-- <div class="px-6 pt-4">
+        <input type="text" class="w-full rounded px-4 py-2.5text-sm font-light bg-gray-800 text-gray-400 placeholder-gray-500" placeholder="Search">
+    </div> -->
+<div class="w-64 h-44 rounded-md" style="background-color:rgb(17, 24, 39)">    
+    <div class="px-6 pt-4 ">
+        <ul class="flex flex-col space-y-2">
+            <li class="text-gray-500 mt-6">
+                <a href="twint" class="inline-block w-full py-2 pl-8 pr-4 rounded hover:bg-gray-300">Panel</a>
+            </li>
+            <li class="text-gray-500">
+                <a href="/twint-data" class="inline-block w-full py-2 pl-8 pr-4 rounded hover:bg-gray-300">Hash</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- <div class="px-6 pt-4">
+        <hr class="border-gray-700"/>
+    </div> -->
+</div>
