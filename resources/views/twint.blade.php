067b3c3327930e363cbf77155ca470da075417a8
jorge asensio alvarez <jokelyn1111@gmail.com>
2022-05-04 11:53:32 +0200

    pruebas en sidebar

--- resources/views/twint.blade.php
+++ resources/views/twint.blade.php
@@ -1,21 +1,21 @@
 <x-app-layout>
     
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <x-slot name="header" >
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
             {{ __('Twint') }}
         </h2>
     </x-slot>
     
-    <div class="grid grid-rows-2 grid-flow-col gap-4 mt-5">
-        <div class="max-w-screen-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="row-span-3 col-span-1">
-                <div class="px-4 sm:px-0">
+    
+        <!-- <div class="max-w-screen-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="row-span-3 col-span-1"> -->
+                <div class="float-left pt-5">
                     @include('twint.sidebar')                
                 </div>
-            </div>
-        </div>
-        <div class="row-span-3 col-span-3">
+            <!-- </div>
+        </div> -->
+        <div class="pt-5">
              @include('twint.panel')
         </div>
-    </div>
+    
 </x-app-layout>
--- resources/views/twint/sidebar.blade.php
+++ resources/views/twint/sidebar.blade.php
@@ -1,19 +1,13 @@
-    <!-- <div class="px-6 pt-4">
-        <input type="text" class="w-full rounded px-4 py-2.5text-sm font-light bg-gray-800 text-gray-400 placeholder-gray-500" placeholder="Search">
-    </div> -->
-    <div class="w-64 h-44 rounded-md" style="background-color:rgb(17, 24, 39)">    
-    <div class="px-6 pt-4 ">
-        <ul class="flex flex-col space-y-2">
-            <li class="text-gray-500 mt-6">
-                <a href="{{route('twint')}}" class="inline-block w-full py-2 pl-8 pr-4 rounded hover:bg-gray-300">Panel</a>
+
+    <div class="w-60 container rounded mx-auto px-10 bg-black pt-5 pb-5  mr-1">
+    
+        
+        <ul class="">
+            <li class="text-gray-500 p-2">
+                <a href="{{route('twint')}}" class=" text-lg rounded hover:bg-gray-300 px-2">Panel</a>
             </li>
-            <li class="text-gray-500">
-                <a href="{{route('twint.data')}}" class="inline-block w-full py-2 pl-8 pr-4 rounded hover:bg-gray-300">Hash</a>
+            <li class="text-gray-500 p-2">
+                <a href="{{route('twint.data')}}" class="text-lg rounded hover:bg-gray-300 px-2">Hash</a>
             </li>
         </ul>
     </div>
-
-    <!-- <div class="px-6 pt-4">
-        <hr class="border-gray-700"/>
-    </div> -->
-</div>
